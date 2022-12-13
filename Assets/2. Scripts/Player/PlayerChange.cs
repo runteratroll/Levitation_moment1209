dbf6633f1f9ecbca4fff4ac4758b2480c9102c40
runteratroll <68888059+runteratroll@users.noreply.github.com>
2022-12-13 16:35:38 +0900

    죽으면 플레이어 자동으로 바뀌는거 만듬

--- Assets/2. Scripts/Player/PlayerChange.cs
+++ Assets/2. Scripts/Player/PlayerChange.cs
@@ -30,7 +30,45 @@ public class PlayerChange : MonoBehaviour
     void Change()
     {
         //플레이어 죽으면 바로 다른걸로 바뀌게
-        if (Input.GetKeyDown(KeyCode.Alpha1) && ch != 0 && playerHealths[0].dead == false)
+
+        if(ch == 0 &&  playerHealths[0].dead)
+        {
+            if(playerHealths[1].dead == false)
+            {
+                ch = 1;
+            }
+            else if (playerHealths[2].dead == false)
+            {
+
+                ch = 2;
+            }
+        }
+        if (ch == 1 && playerHealths[1].dead)
+        {
+            if (playerHealths[0].dead == false)
+            {
+                ch = 0;
+            }
+            else if (playerHealths[2].dead == false)
+            {
+
+                ch = 2;
+            }
+        }
+        if (ch == 2 && playerHealths[2].dead)
+        {
+            if (playerHealths[0].dead == false)
+            {
+                ch = 0;
+            }
+            else if (playerHealths[1].dead == false)
+            {
+
+                ch = 1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && ch != 0 && playerHealths[0].dead == false || (ch == 0 && playerHealths[0].dead == false))
         {
             
             player[1].SetActive(false);
@@ -41,7 +79,7 @@ public class PlayerChange : MonoBehaviour
             ch = 0;
             playerSelectFrame.Select(ch);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2) && ch != 1 && playerHealths[1].dead == false)
+        if (Input.GetKeyDown(KeyCode.Alpha2) && ch != 1 && playerHealths[1].dead == false || (ch == 1 && playerHealths[1].dead == false))
         {
 
             
@@ -51,7 +89,7 @@ public class PlayerChange : MonoBehaviour
             ch = 1;
             playerSelectFrame.Select(ch);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3) && ch != 2 && playerHealths[2].dead == false)
+        if (Input.GetKeyDown(KeyCode.Alpha3) && ch != 2 && playerHealths[2].dead == false || (ch == 2 && playerHealths[2].dead == false))
         {
             
             player[0].SetActive(false);
