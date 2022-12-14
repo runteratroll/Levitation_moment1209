2788c6edaa8a273bca0448fe31b93f0c0adfc264
snrnsrk5 <77601112+snrnsrk5@users.noreply.github.com>
2022-12-14 15:11:44 +0900

    ㄴㅇㄴㅇ

--- Assets/2. Scripts/Behaviour/Main410mmProejctilAtk.cs
+++ Assets/2. Scripts/Behaviour/Main410mmProejctilAtk.cs
@@ -16,10 +16,11 @@ public class Main410mmProejctilAtk : CollisionProjectileAtk
     {
         base.Start();
         damageObj = Instantiate(damageAround, transform.position, Quaternion.identity);
-    
+        
         
 
-        damageObj.transform.position = target.transform.position; //��ȯ�ǰ�
+        damageObj.transform.position = target.transform.position  + Vector3.up * 0.14f ; //��ȯ�ǰ�
+
 
     }
 
