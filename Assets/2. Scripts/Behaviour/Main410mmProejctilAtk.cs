3bc3b8a84897e511ebbedc109fbb245dd0e4eb94
runteratroll <68888059+runteratroll@users.noreply.github.com>
2022-12-19 15:50:27 +0900

    데미지 어라운드 탄퍼짐 만듬

--- Assets/2. Scripts/Behaviour/Main410mmProejctilAtk.cs
+++ Assets/2. Scripts/Behaviour/Main410mmProejctilAtk.cs
@@ -12,22 +12,82 @@ public class Main410mmProejctilAtk : CollisionProjectileAtk
     private int range;
     private int idx;
 
+
+
     protected override void Start()
     {
-        base.Start();
+        getFlagProjectileCollid = false;
+        rigidbody = GetComponent<Rigidbody>();
+        //그냥 target도 있으니까 
+
+        //그냥 데미지만 넣어도 괜찮을 거같소
+        //만약 소유자가 있다면 
+        if (projectileParents != null)
+        {
+            Collider collidProjectile = GetComponent<Collider>();
+            Collider[] collidParents = projectileParents.GetComponentsInChildren<Collider>();
+            foreach (Collider collider in collidParents)
+            {
+                Physics.IgnoreCollision(collidProjectile, collider);
+            }
+        }
 
 
-        InstanceDamageAround();
+        if (ObjProjectileStartEffect != null)
+        {
+            var projectileStartEffect = Instantiate(this.ObjProjectileStartEffect, transform.position, Quaternion.identity);
+
+
+
+            projectileStartEffect.transform.forward = gameObject.transform.forward;
+
+            ParticleSystem particleSystem = projectileStartEffect.GetComponent<ParticleSystem>();
+
+            if (particleSystem != null)
+            {
+                Destroy(projectileStartEffect, particleSystem.main.duration * 20);
+            }
+            else
+            {
+                ParticleSystem particleSystemChild = projectileStartEffect.transform.GetChild(0).GetComponent<ParticleSystem>();
+
+                Destroy(projectileStartEffect, particleSystemChild.main.duration * 20);
+            }
+        }
+
+
+        //사운드 관련부분
+        if (projectileStartClip != null && GetComponent<AudioSource>())
+        {
+            GetComponent<AudioSource>().PlayOneShot(projectileStartClip);
+        }
+
+
+        //타겟이 플레이어라면, 발사체가 바라보는 방향을 타겟으로 한다. 이걸 업데이트로 하면은 유도탄같은 개념인가
+        if (target != null)
+        {
+            //Vector3 vecProjectile = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+            ////vecProjectile.y += 0.9f;
+
+
+
+
+            //transform.LookAt(vecProjectile);
+        }
+
+        
+
+
 
     }
 
 
-    public void InstanceDamageAround()
+    public void InstanceDamageAround(Vector3 tagetRandom)
     {
         Debug.Log("소환되니?");
         damageObj = Instantiate(damageAround);
 
-        damageObj.transform.position = target.transform.position + Vector3.up * 0.2f; //��ȯ�ǰ�
+        damageObj.transform.position = new Vector3(tagetRandom.x, tagetRandom.y * 1.2f, tagetRandom.z); //��ȯ�ǰ�
         damageObj.transform.localScale = new Vector3(damageObj.transform.localScale.x, 0.01f, damageObj.transform.localScale.z);
 
         //랜덤 레인지 
---
+++ Assets/2. Scripts/Behaviour/bullet/multiProjectilAtkBehaviour.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class multiProjectilAtkBehaviour : AtkBehaviour
+{
+    public AudioSource audioSource;
+    public ParticleSystem particle;
+    public float pitch;
+
+
+    public override void callAtkMotion(GameObject target = null, Transform posAtkStart = null)
+    {
+        if (target == null && posAtkStart != null) //Ÿ���� ����, �߻���ġ����
+        {
+            return;
+        }
+        //Ÿ���� �����ִ� �Ű��� 
+        Debug.Log("멀티슛 되니?");
+
+        if (audioSource != null)
+        {
+
+
+            pitch = Random.RandomRange(0.8f, 1.2f);
+            audioSource.pitch = pitch;
+
+            audioSource.Play();
+        }
+
+        if (particle != null)
+        {
+            particle.Play();
+        }
+
+
+        Vector3 vecProjectile = posAtkStart?.position ?? transform.position; //�߻���ġ�� ���̶�� �ڱ���ġ����
+
+        if (atkEffectPrefab != null)
+        {
+
+
+
+            //�������� ������� 
+            GameObject objProjectile = GameObject.Instantiate<GameObject>(atkEffectPrefab, vecProjectile, Quaternion.identity);
+            //�ٵ� �� �̰� �� rprojedtil�� �����ϱ� Ÿ�ٵ� ������
+
+            Vector3 l_vector = target.transform.position - transform.position;
+            Quaternion ro = Quaternion.LookRotation(l_vector).normalized; //position���� ����
+            objProjectile.transform.forward = transform.forward; //������ �θ��� forward�� ����. �� �����ΰ�
+            objProjectile.transform.rotation = ro;
+
+            
+
+         
+
+            Main410mmProejctilAtk[] projectile = objProjectile.GetComponentsInChildren<Main410mmProejctilAtk>(); //�������� �̰� ������ �̝��� �ϰ�
+            Transform[] childs = objProjectile.GetComponentsInChildren<Transform>();
+            for (int i = 0; i < childs.Length; i++)
+            {
+                childs[i].parent = null;
+            }                                                                                       //bulletMove���� CollisionprojectilAtk�� ��Ӿȹ����ϱ� �ù�
+                                                                                                    //projectile.gameObject.AddComponent<peripheralRange>();
+
+            for (int i = 0; i < projectile.Length; i++)
+            {
+                if (projectile[i] != null)
+                {
+                    projectile[i].projectileParents = this.gameObject;  //�θ� ����� 
+                    projectile[i].target = target;
+                    projectile[i].attackBehaviour = this;
+
+                    Vector3 vec = new Vector3(Random.Range(target.transform.position.x - 3f, target.transform.position.x + 3f),
+                        target.transform.position.y, Random.Range(target.transform.position.z - 3f, target.transform.position.z + 3f));
+                    projectile[i].transform.LookAt(vec);
+
+                    projectile[i].InstanceDamageAround(vec);
+                    //add 해줘야 겠다.
+                    //peripheralRange peripheral = new peripheralRange(target, projectile.gameObject);
+
+                }
+            }
+
+       
+
+            Destroy(objProjectile);
+            //타겟위치 넘겨주고 adad( target,projecitle,
+            //그곳에 빨간 표시 해주고, 그부분에 자기가 쏜 불렛이 들어가면 physics콜라이더
+            //bullet에 그걸 넣고
+        }
+
+
+        //타겟의 위치에 범위 그리기
+
+        nowAtkCoolTime = 0.0f;
+    }
+
+}
